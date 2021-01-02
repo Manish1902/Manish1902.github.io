3e44b528d485e2506ba9ab1d0ddc6fa04fea1bdb
Manish1902 <56498641+Manish1902@users.noreply.github.com>
2021-01-02 11:29:46 +0530

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,197 @@
+
+
+
+//Array of Objects
+const quizA = [
+ {
+ 	q:"English = 'four', Hindi = '' ",
+ 	options:[' चतुर् ',' द्वि ',' पञ्चन् ',' अन्य '],
+ 	answer:0
+ },
+ {
+ 	q:"English = 'you', Hindi = '' ",
+ 	options:['स','त्वम्','अहम्','वयम्'],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'many', Hindi = '' ",
+ 	options:['अल्प','किञ्चिद्','बहु','अन्य'],
+ 	answer:2
+ },
+ {
+ 	q:"English = thin', Hindi = '' ",
+ 	options:['घन','तनु','अल्प'],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'thick', Hindi = '' ",
+ 	options:['घन','तनु','अल्प',' अन्य '],
+ 	answer:0
+ },
+ {
+ 	q:"English = 'who', Hindi = '' ",
+ 	options:['अत्र','तत्र ','इदम्','क'],
+ 	answer:3
+ },
+ {
+ 	q:"English = 'this', Hindi = '' ",
+ 	options:['अत्र','तत्र ','इदम्','क'],
+ 	answer:2
+ },
+ {
+ 	q:"English = 'that', Hindi = '' ",
+ 	options:['अत्र','तत्र ','इदम्','क'],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'two', Hindi = '' ",
+ 	options:[' चतुर् ',' द्वि ',' पञ्चन् ',' अन्य '],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'all', Hindi = '' ",
+ 	options:['अल्प','सर्व','बहु','अन्य'],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'small', Hindi = '' ",
+ 	options:[' महत् ',' अल्प ',' उरु ',' घन '],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'snake', Hindi = '' ",
+ 	options:['सर्प',' पशु ',' मत्स्य ',' अन्य '],
+ 	answer:0
+ },
+ {
+ 	q:"English = 'Dog', Hindi = '' ",
+ 	options:['सर्प',' पशु ',' मत्स्य ',' श्वन् '],
+ 	answer:3
+ },
+ {
+ 	q:"English = 'Fish', Hindi = '' ",
+ 	options:['सर्प',' पशु ',' मत्स्य ',' श्वन् '],
+ 	answer:2
+ },
+ {
+ 	q:"English = 'Fruit', Hindi = '' ",
+ 	options:['बीज',' फल ',' मूल '],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'Meat', Hindi = '' ",
+ 	options:[' मांस ',' चर्मन् ',' मेदस् ',' अन्य '],
+ 	answer:0
+ },
+ {
+ 	q:"English = 'Forest', Hindi = '' ",
+ 	options:[' वृक्ष ',' वन ',' दण्ड ',' पत्त्र '],
+ 	answer:1
+ },
+ {
+ 	q:"English = 'Tree', Hindi = '' ",
+ 	options:[' वृक्ष ',' वन ',' दण्ड ',' पत्त्र '],
+ 	answer:0
+ },
+ {
+ 	q:"English = 'Leaf', Hindi = '' ",
+ 	options:[' वृक्ष ',' वन ',' दण्ड ',' पत्त्र '],
+ 	answer:3
+ },
+ {
+ 	q:"English = 'Mouth', Hindi = '' ",
+ 	options:[' मुख ',' शिरस् ',' केश  ',' अक्षि '],
+ 	answer:0
+ },
+ {
+ 	q:"English = 'Hair', Hindi = '' ",
+ 	options:[' मुख ',' शिरस् ',' केश  ',' अक्षि '],
+ 	answer:2
+ },
+ {
+ 	q:"English = 'Eye', Hindi = '' ",
+ 	options:[' मुख ',' शिरस् ',' केश ',' अक्षि '],
+ 	answer:3
+ },
+ {
+ 	q:"English = 'tooth', Hindi = '' ",
+ 	options:[' मुख ',' शिरस् ',' दन्त ',' अक्षि '],
+ 	answer:2
+ },
+ {
+ 	q:"English = 'hand', Hindi = '' ",
+ 	options:[' पद ',' शिरस् ',' हस्त',' नख '],
+ 	answer:2
+ },
+ {
+ 	q:"English = 'fingernail', Hindi = '' ",
+ 	options:[' जानु',' शिरस् ',' पद ',' नख '],
+ 	answer:3
+ }
+]
+
+const quizB = [
+ {
+ 	q:'who is the father of nation',
+ 	options:['gandhi','rahul ','karan','mohan'],
+ 	answer:0
+ },
+ {
+ 	q:'didi ka birthday',
+ 	options:['21 aug','15 aug','26 jan','24 feb'],
+ 	answer:3
+ },
+ {
+ 	q:'republic day',
+ 	options:['21 aug','15 aug','26 jan','14 jan'],
+ 	answer:2
+ },
+ {
+ 	q:'bhaiya ka birthday',
+ 	options:['1 jan','1 aug','16'],
+ 	answer:1
+ },
+ {
+ 	q:'abc_ef',
+ 	options:['i','g','n','d'],
+ 	answer:3
+ },
+ {
+ 	q:'manish ka birthday',
+ 	options:['19 aug','g','n','d'],
+ 	answer:0
+ }
+]
+
+const quizC = [
+ {
+ 	q:'who was munna bhaiya',
+ 	options:['son of kalen bhaiya','rahul ','karan','mohan'],
+ 	answer:0
+ },
+ {
+ 	q:'mummy ka birthday',
+ 	options:['21 aug','15 dec','26 jan','14 jan'],
+ 	answer:1
+ },
+ {
+ 	q:'republic day',
+ 	options:['21 aug','15 aug','26 jan','14 jan'],
+ 	answer:2
+ },
+ {
+ 	q:'5+6',
+ 	options:['12','11','16'],
+ 	answer:1
+ },
+ {
+ 	q:'abc_ef',
+ 	options:['i','g','n','d'],
+ 	answer:3
+ },
+ {
+ 	q:'papa ka birthday',
+ 	options:['1 july','g','n','d'],
+ 	answer:0
+ }
+]
